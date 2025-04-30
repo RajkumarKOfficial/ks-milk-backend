92b841a6aa5906e202199c5ee47db6743b1b27bb
RajkumarKOfficial <Kumar.RajkumarOfficial@gmail.com>
2025-04-30 22:51:17 +0530

    Re Updation Done

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 // PostgreSQL connection
 const pool = new Pool({
-  connectionString: 'your_connection_string_here',
+  connectionString: 'postgresql://ks_user:Pg6s7ATF979crVWlRSwsj03Z7pYFunnF@dpg-d095jr3ipnbc7398corg-a/ks_milk_db',
   ssl: { rejectUnauthorized: false }
 });
 
